58765efebeedfee704a21cb73c4f091761c301f8
Abdelrhman Essam <89538266+AbdelrhmanEssam74@users.noreply.github.com>
2023-09-05 18:16:28 +0300

    Add files via upload

---
+++ ToDo list/index.php
@@ -0,0 +1,80 @@
+<?php
+
+require "conn.php";
+
+
+$data = $conn->query("SELECT * FROM tasks");
+$data->execute();
+$rows = $data->fetchAll(PDO::FETCH_OBJ);
+
+$completed = $conn->query("SELECT * FROM Tasks_completed");
+$completed->execute();
+$completed_rows = $completed->fetchAll(PDO::FETCH_OBJ);
+
+
+?>
+
+<?php include "header.php"; ?>
+<div class="container">
+    <form method="POST" action="insert.php" class="form-inline" id="user_form">
+
+        <div class="form-group mx-sm-3 mb-2">
+            <input name="mytask" type="text" class="form-control" id="task" placeholder="enter task">
+        </div>
+
+        <input type="submit" name="submit" class="btn btn-primary" value="Add Task" />
+    </form>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Task Name</th>
+                <th>Time</th>
+                <th>Done</th>
+                <th>Update</th>
+                <th>Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($rows as $row) : ?>
+                <tr>
+
+                    <td><?php echo $row->id; ?></td>
+                    <td><?php echo $row->task_name; ?></td>
+                    <td><?php echo $row->created_at; ?></td>
+                    <td><a href="done.php?del_id=<?php echo $row->id; ?>" class="btn btn-success">Done</a></td>
+                    <td><a href="update.php?upd_id=<?php echo $row->id; ?>" class="btn btn-warning">Update</a></td>
+                    <td><a href="delete.php?del_id=<?php echo $row->id; ?>" class="btn btn-danger">Delete</a></td>
+                </tr>
+            <?php endforeach; ?>
+
+
+        </tbody>
+    </table>
+    <h3>Tasks completed</h3>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Task Name</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($completed_rows as $r) : ?>
+                <tr>
+
+                    <td><?php echo $r->id; ?></td>
+                    <td><?php echo $r->task_name; ?></td>
+                </tr>
+            <?php endforeach; ?>
+
+
+        </tbody>
+    </table>
+</div>
+
+
+
+
+<?php include "footer.php"; ?>
